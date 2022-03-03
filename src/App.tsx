586b340e278113626d3a14ef3e03520ace05702e
Brandon Aguiar <brandonaguiar@wifi-roaming-128-4-156-40.wifi.udel.edu>
2022-03-03 12:42:03 -0500

    finished 3/5 task 8

--- src/App.tsx
+++ src/App.tsx
@@ -14,52 +14,6 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-<<<<<<< HEAD
-            <div style={{ background: "cyan" }}>
-                <h1> My First Personal Header </h1>
-            </div>
-            <p>Brandon Aguiar. Hello World.</p>
-            <img
-                src="https://thumbs.dreamstime.com/b/golden-retriever-dog-21668976.jpg"
-                alt="A picture of a dog"
-            />
-            <div>
-                <Button onClick={() => console.log("Hello World!")}>
-                    Log Hello World
-                </Button>
-            </div>
-            <ol>
-                <li>Dogs are fun</li>
-                <li>Dogs are cute</li>
-                <li>Dogs are happy</li>
-            </ol>
-            <div>
-                <Container>
-                    <Row>
-                        <Col>
-                            Dogs are so much better...
-                            <div
-                                style={{
-                                    backgroundColor: "red",
-                                    width: "600px",
-                                    height: "20px"
-                                }}
-                            ></div>
-                        </Col>
-                        <Col>
-                            ...And cats are so much worse
-                            <div
-                                style={{
-                                    backgroundColor: "red",
-                                    width: "600px",
-                                    height: "20px"
-                                }}
-                            ></div>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-=======
             <hr></hr>
             <Counter></Counter>
             <hr />
@@ -72,7 +26,6 @@ function App(): JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
->>>>>>> upstream/task-state
         </div>
     );
 }
--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,38 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attempts, changeAttempts] = useState<number>(4);
+    const [progress, checkProgress] = useState<boolean>(false);
+
+    function PutInProgress(): void {
+        checkProgress(!progress);
+    }
+
+    function takeOutProgress(): void {
+        checkProgress(!progress);
+        changeAttempts(attempts - 1);
+    }
+
+    function increaseAttempts(): void {
+        changeAttempts(attempts + 1);
+    }
+
+    return (
+        <div>
+            {" "}
+            <Button
+                onClick={PutInProgress}
+                disabled={progress || attempts === 0}
+            >
+                Start Quiz
+            </Button>
+            <Button onClick={takeOutProgress} disabled={!progress}>
+                Stop Quiz
+            </Button>
+            <Button onClick={increaseAttempts} disabled={progress}>
+                Mulligan
+                {attempts}
+            </Button>
+        </div>
+    );
 }

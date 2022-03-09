063ddadff13b5966689d6fe5db921a3c2eb4487a
Brandon Aguiar <brandonaguiar@wifi-roaming-128-4-85-89.wifi.udel.edu>
2022-03-08 19:50:47 -0500

    Finished choose team

--- src/bad-components/ChooseTeam.tsx
+++ src/bad-components/ChooseTeam.tsx
@@ -10,22 +10,25 @@ const PEOPLE = [
     "Margaret Hamilton"
 ];
 
+//interface myFunc {
+//   team: string[];
+//    setTeam: (team: string[]) => void;
+//}
+
 export function ChooseTeam(): JSX.Element {
     const [allOptions, setAllOptions] = useState<string[]>(PEOPLE);
     const [team, setTeam] = useState<string[]>([]);
 
-    function chooseMember() {
-        /*
-        if (!team.includes(newMember)) {
-            team.push(newMember);
+    function chooseMember(newMem: string) {
+        const newTeam = [...team];
+        if (!newTeam.includes(newMem)) {
+            newTeam.push(newMem);
         }
-        */
+        setTeam(newTeam);
     }
 
-    function clearTeam() {
-        /*
-        team = [];
-        */
+    function clearTeam(): void {
+        setTeam([]);
     }
 
     return (
@@ -36,7 +39,10 @@ export function ChooseTeam(): JSX.Element {
                     {allOptions.map((option: string) => (
                         <div key={option} style={{ marginBottom: "4px" }}>
                             Add{" "}
-                            <Button onClick={chooseMember} size="sm">
+                            <Button
+                                onClick={() => chooseMember(option)}
+                                size="sm"
+                            >
                                 {option}
                             </Button>
                         </div>

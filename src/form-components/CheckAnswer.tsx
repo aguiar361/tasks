395c2a6f0f4d783b6ce666c632f003acc8a5ec9d
Brandon Aguiar <brandonaguiar@wifi-roaming-128-4-128-162.wifi.udel.edu>
2022-03-10 20:47:05 -0500

    Finished CheckAnswer

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,33 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [given, setIfEqual] = useState<string>("");
+
+    function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+        setIfEqual(event.target.value);
+    }
+
+    /**function checkEqual(): string {
+        if (given === expectedAnswer) {
+            return "✔️";
+        } else {
+            return "❌";
+        }
+    }**/
+
     return (
         <div>
             <h3>Check Answer</h3>
+            <Form.Group controlId="answerBox">
+                <Form.Label>User Answer:</Form.Label>
+                <Form.Control value={given} onChange={updateAnswer} />
+            </Form.Group>
+            <div>{given === expectedAnswer ? "✔️" : "❌"}</div>
         </div>
     );
 }

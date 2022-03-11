cd0648ceab51f8787a50394f6cfe64111a250bc6
Brandon Aguiar <brandonaguiar@wifi-roaming-128-4-108-24.wifi.udel.edu>
2022-03-11 12:12:36 -0500

    Finished second function

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,43 @@
+import { isDisabled } from "@testing-library/user-event/dist/utils";
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [attempts, setHasLeft] = useState<number>(3);
+    const [requesting, updateRequest] = useState<string>("0");
+
     return (
         <div>
             <h3>Give Attempts</h3>
+            <p>Attempts Left: {attempts}</p>
+            <Form.Group controlId="formNumAttempts">
+                <Form.Label column sm={1}>
+                    {" "}
+                    Number of attempts requested:
+                </Form.Label>
+                <Form.Control
+                    type="number"
+                    value={requesting}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        updateRequest(event.target.value)
+                    }
+                />
+            </Form.Group>
+            <div>
+                <Button
+                    onClick={() => setHasLeft(attempts - 1)}
+                    disabled={attempts <= 0 ? true : false}
+                >
+                    Use
+                </Button>
+                <Button
+                    onClick={() =>
+                        setHasLeft(parseInt(requesting) + attempts || attempts)
+                    }
+                >
+                    Gain
+                </Button>
+            </div>
         </div>
     );
 }
